626c6003a29d1b1f7e4d85b142088513d0d635f0
Mikael Goldmann <migo@spotify.com>
2020-04-05 16:36:01 +0200

    TodoItem component added

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class TodoItem extends React.Component {
+    render() {
+        const {todo} = this.props
+        return (
+            <li>{todo.title}</li>
+        )
+    }
+}
+
+export default TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,13 @@
 import React from "react"
+import TodoItem from "./TodoItem";
 
 class TodoList extends React.Component {
     render() {
-        const {todos} = this.props
+        const {todos} = this.props;
         return (
             <React.Fragment>
                 {
-                    todos.map(todo => <li key={todo.id}>{todo.title}</li>)
+                    todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)
                 }
             </React.Fragment>
         )
