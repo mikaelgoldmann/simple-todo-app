887dd428bb8ac1d3f7c53b0fba45f0b9f8f74599
Mikael Goldmann <migo@spotify.com>
2020-04-05 16:52:18 +0200

    Functional TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,13 @@
 import React from "react"
 import TodoItem from "./TodoItem";
 
-class TodoList extends React.Component {
-    render() {
-        const {todos} = this.props;
-        return (
-            <React.Fragment>
-                {
-                    todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)
-                }
-            </React.Fragment>
-        )
-    }
-}
+const TodoList = ({todos}) =>
+    (
+        <React.Fragment>
+            {
+                todos.map(todo => <TodoItem key={todo.id} todo={todo}/>)
+            }
+        </React.Fragment>
+    );
 
 export default TodoList
