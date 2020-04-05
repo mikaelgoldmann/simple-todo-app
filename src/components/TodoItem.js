df56a2608f8e8760a35f8ece8297c0e808a6d991
Mikael Goldmann <migo@spotify.com>
2020-04-05 16:50:32 +0200

    Functinal TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,9 @@
 import React from "react"
 
-class TodoItem extends React.Component {
-    render() {
-        const {todo} = this.props
-        return (
-            <li>{todo.title}</li>
-        )
-    }
-}
+const TodoItem = ({todo}) => {
+    return (
+        <li>{todo.title}</li>
+    )
+};
 
 export default TodoItem
