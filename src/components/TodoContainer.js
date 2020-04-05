550cd6afcfaea053596c573cd5367a8039d1bff5
Mikael Goldmann <migo@spotify.com>
2020-04-05 15:55:16 +0200

    Revert "TodoContainer using functional def"
    This reverts commit a52c97b1b8555819400b31810d1166d759afa725.
    
    We need a class since we are adding state.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,11 +1,14 @@
 import React from "react"
 
-const TodoContainer = () => {
-    return (
-        <React.Fragment>
-            <h1>Hello from Create React App</h1>
-            <p>I am in a React Component!</p>
-        </React.Fragment>
-    )
-};
+class TodoContainer extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello from Create React App</h1>
+                <p>I am in a React Component!</p>
+            </React.Fragment>
+        )
+    }
+}
+
 export default TodoContainer
