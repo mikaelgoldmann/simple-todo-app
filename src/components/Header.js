ff0abd5b55d3f72f377d65d5e60c8daa18481c10
Mikael Goldmann <migo@spotify.com>
2020-04-05 20:32:50 +0200

    Put header style in variable

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react"
 
+const headerStyle = {fontSize: "25px", marginBottom: "15px"};
+
 const Header = () => {
     return (
         <header>
-            <h1 style={{fontSize: "25px", marginBottom: "15px"}}>Simple Todo App</h1>
+            <h1 style={headerStyle}>Simple Todo App</h1>
             <p style={{fontSize: "19px"}}>Please add to-dos item(s) through the input field</p>
         </header>
     )
-}
+};
 
 export default Header
