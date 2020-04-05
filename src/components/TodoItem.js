c1ea1f531753032048f6a8633cd2a807d0452331
Mikael Goldmann <migo@spotify.com>
2020-04-05 20:36:43 +0200

    Style completed items with special style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,18 @@
 import React from "react"
 
+const completedStyle = {
+    fontStyle: "italic",
+    color: "#d35e0f",
+    opacity: 0.4,
+    textDecoration: "line-through",
+};
+
 const TodoItem = ({todo, completedChange, delTodo}) => {
     return (
         <li className="todo-item">
             <input type="checkbox" checked={todo.completed}
                    onChange={() => completedChange(todo.id)}/>
-            {todo.title}
+            <span style={todo.completed ? completedStyle : null}>{todo.title}</span>
             <button onClick={() => delTodo(todo.id)}>Delete</button>
         </li>
     )
