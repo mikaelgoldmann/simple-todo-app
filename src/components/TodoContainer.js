2a12d9d2ede8b40e5a0b3ac52ce9af9785df71f2
Mikael Goldmann <migo@spotify.com>
2020-04-11 17:02:07 +0200

    Use backend.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,45 +2,57 @@ import React from "react";
 import TodoList from "./TodoList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
-import {v4 as uuidv4} from "uuid";
+import axios from "axios";
+
+const baseUrl = 'http://127.0.0.1:5000';
 
 class TodoContainer extends React.Component {
     state = {
-        todos: [
-            {
-                id: uuidv4(),
-                title: "Setup development environment",
-                completed: true
-            },
-            {
-                id: uuidv4(),
-                title: "Develop website and add content",
-                completed: false
-            },
-            {
-                id: uuidv4(),
-                title: "Deploy to live server",
-                completed: false
-            }
-        ]
+        todos: []
     };
 
     toggleComplete = i => {
-        const newTodos = this.state.todos.map(todo => {
-            if (todo.id === i) {
-                todo.completed = !todo.completed;
-            }
-            return todo;
-        });
-        this.setState({todos: newTodos});
+        const found = this.state.todos.find(todo => todo['id'] === i);
+        if (found) {
+            const newCompleted = !found.completed;
+            axios.post(baseUrl + "/todo/" + i, {completed: newCompleted})
+                .then(reply => {
+                          const newTodos = this.state.todos.map(todo => {
+                              if (todo.id === i) {
+                                  todo.completed = !todo.completed;
+                              }
+                              return todo;
+                          });
+                          this.setState({todos: newTodos});
+                      }
+                )
+                .catch(reason => alert("Failed to update complete: " + reason))
+        } else {
+            alert("Could not find todo with id " + i);
+        }
     };
 
     delTodo = i =>
-        this.setState({todos: [...this.state.todos.filter(todo => todo.id !== i)]});
+        axios.delete(baseUrl + "/todo/" + i)
+            .then(response =>
+                      this.setState({todos: [...this.state.todos.filter(todo => todo.id !== i)]}))
+            .catch(reason =>
+                       alert("Failed to delete item " + reason));
 
-    addTodo = title => this.setState(
-        {todos: [...this.state.todos, {id: uuidv4(), title: title, completed: false}]}
-    );
+    addTodo = title =>
+        axios.post(baseUrl + '/todo', {title: title, completed: false})
+            .then(response => this.setState({
+                                                todos: [...this.state.todos,
+                                                        response.data]
+                                            }
+            ));
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:5000/todo')
+            .then(response =>
+                      this.setState({todos: response.data})
+            );
+    }
 
     render() {
         const {todos} = this.state;
