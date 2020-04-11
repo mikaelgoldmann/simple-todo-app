648870f062da81ca6b20ed5574ec8e3bf5b14455
Mikael Goldmann <migo@spotify.com>
2020-04-11 19:35:31 +0200

    Header as function with useEffect and useRef

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,35 @@
-import React, {Component} from "react"
+import React, {useEffect, useRef} from "react"
 
 const headerStyle = {fontSize: "25px", marginBottom: "15px"};
 
-class Header extends Component {
+const Header = props => {
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.headerSpan !== this.props.headerSpan) {
-            const red = Math.floor(Math.random() * 256);
-            const green = Math.floor(Math.random() * 256);
-            const blue = Math.floor(Math.random() * 256);
-            const bgColor = `rgb(${red},${green},${blue})`;
+    const uglyMessage = () => {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        const bgColor = `rgb(${red},${green},${blue})`;
 
-            document.getElementById("inH1").innerHTML = "clicked";
-            document.getElementById("inH1").style.backgroundColor = bgColor;
-        }
-    }
-
-    render() {
-        return (
-            <header>
-                <h1 style={headerStyle}>Simple Todo App <span id="inH1"></span></h1>
-                <p style={{fontSize: "19px"}}>Please add to-dos item(s) through the input field</p>
-            </header>
-        )
+        document.getElementById("inH1").innerHTML = "clicked";
+        document.getElementById("inH1").style.backgroundColor = bgColor;
     };
-}
+
+    const isInitialMount = useRef(true);
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            uglyMessage();
+        }
+    }, [props.headerSpan]);
+
+    return (
+        <header>
+            <h1 style={headerStyle}>Simple Todo App <span id="inH1"></span></h1>
+            <p style={{fontSize: "19px"}}>Please add to-dos item(s) through the input field</p>
+        </header>
+    );
+};
 
 export default Header
